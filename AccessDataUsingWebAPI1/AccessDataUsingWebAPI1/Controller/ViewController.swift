5178b64f568d577387ca0353591bfe8020af0f67
Pradip Khandare <pradipkhandare111@gmail.com>
2024-01-18 11:33:25 +0530

    Add files via upload

---
+++ AccessDataUsingWebAPI1/AccessDataUsingWebAPI1/Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AccessDataUsingWebAPI1
+//
+//  Created by NTS on 29/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var labelId: UILabel!
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelUserid: UILabel!
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+}
+
